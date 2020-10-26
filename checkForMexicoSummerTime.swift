a0bca42b509c466519fd91857ca51d95f0c7be1d
Gerardo Naranjo <55220771+GeraDNG@users.noreply.github.com>
2020-10-26 14:05:15 -0600

    Swift function

---
+++ checkForMexicoSummerTime.swift
@@ -0,0 +1,14 @@
+private var year : Int = 2020
+private var month : Int = 4
+private var day : Int = 6
+private var hour: Int = 23
+
+private func checkSummerTime() -> Bool {
+    if month > 4 && month < 10 { return true }
+    else if month < 4 || month > 10 { return false }
+    else if month == 4 && ((hour+(24*day)) >= (3+24*(7-(5*year/4+4)%7))) { return true }
+    else if month == 10 && ((hour+(24*day)) < (3+24*(31-(5*year/4+1)%7))) { return true }
+    return false
+}
+
+print(checkSummerTime())
