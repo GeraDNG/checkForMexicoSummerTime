3274a09315215dbc5b93cb7b3fbb26d4323a8f69
Gerardo Naranjo <55220771+GeraDNG@users.noreply.github.com>
2020-10-26 14:07:33 -0600

    Function for Arduino

---
+++ checkForMexicoSummerTime.ino
@@ -0,0 +1,14 @@
+int year = 2020;
+int month = 4;
+int day = 6;
+int hour = 23;
+
+bool checkSummerTime(void) {
+    if month > 4 && month < 10 { return true }
+    else if month < 4 || month > 10 { return false }
+    else if month == 4 && ((hour+(24*day)) >= (3+24*(7-(5*year/4+4)%7))) { return true }
+    else if month == 10 && ((hour+(24*day)) < (3+24*(31-(5*year/4+1)%7))) { return true }
+    return false
+}
+
+Serial.println(checkSummerTime())
